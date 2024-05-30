f3d64c6812b27493b67719c4cbde698f1eea2cb1
AnkurGupta97 <159519198+AnkurGupta97@users.noreply.github.com>
2024-05-30 19:01:48 +0530

    Add files via upload

---
+++ Car_sales.sql
@@ -0,0 +1,37 @@
+use project_db;
+select * from car_sales;
+-- Total sales------
+select sum(price) as Total_sales from car_sales;
+-- Average Price -----
+Select avg(price) as Average_price_of_car from car_sales;
+-- Number of cars sold------
+Select count(Car_id) as Total_cars_sold from car_sales;
+-- Dealer Region wise sales-----
+select Dealer_Region,sum(Price)/1000000 as Sales_in_millions
+ from car_sales group by Dealer_Region order by sum(Price) desc;
+ -- Gender wise car sales count---
+ select Gender ,count(Car_id) as cars_sold from car_sales group by Gender;
+ -- Company wise sales--
+select Company,sum(Price) as Total_sales from car_sales group by Company 
+order by sum(Price) desc;
+ 
+
+
+
+
+
+
+select sum(price)as a from car_sales where EXTRACT(YEAR FROM Date)='2020';
+
+
+
+SELECT DATE_FORMAT(Date, '%d/%m/%Y') FROM car_sales;
+select * from car_sales where year(Date) = 2020;
+
+SELECT EXTRACT(YEAR FROM Date) AS year, SUM(Price) AS total_sales
+FROM car_sales
+GROUP BY EXTRACT(YEAR FROM Date);
+
+
+
+
